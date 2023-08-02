9e8dbd2c238712955f45b3963916830b84a7edc0
Evan Swanson <evan.sundelius@gmail.com>
2023-08-02 16:02:22 -0500

    Add interactivity

--- canvas.js
+++ canvas.js
@@ -26,6 +26,21 @@ const c = canvas.getContext('2d')
 // c.strokeStyle = 'magenta'
 // c.stroke()
 
+
+//Event Listeners :::
+let mouse = {
+    x: undefined,
+    y: undefined
+}
+const maxRadius = 90
+const minRadius = 25
+const affectRadius = 50
+const growthSpeed = 2
+window.addEventListener('mousemove', (event) => {
+    mouse.x = event.x
+    mouse.y = event.y
+})
+
 //Animation
 const characters = ['0','1','2','3','4','5','6','7','8','9','A','B','C','D','E','F']
 const generateCode = () => {
@@ -47,8 +62,8 @@ function Circle(x, y, dx, dy, radius) {
     this.draw = function() {
         c.beginPath()
         c.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false)
-        c.strokeStyle = this.color
-        c.stroke()
+        c.fillStyle = this.color
+        c.fill()
     }
     this.update = function() {
     if (this.x + this.radius + 8 > innerWidth || this.x < this.radius) {
@@ -59,6 +74,16 @@ function Circle(x, y, dx, dy, radius) {
     }
     this.x += this.dx
     this.y += this.dy
+
+    //Interactivity
+    if (mouse.x - this.x < affectRadius && mouse.x - this.x > -affectRadius &&
+        mouse.y - this.y < affectRadius && mouse.y - this.y > -affectRadius) {
+            if (this.radius < maxRadius) {
+                this.radius += growthSpeed
+            }
+    } else if (this.radius > minRadius) {
+        this.radius -= growthSpeed
+    }
 
     this.draw()
     }
